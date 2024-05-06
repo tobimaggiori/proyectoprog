1bd44eb42e66834628cd817ff89f8c9f62a473c4
milisotelo <109885441+milisotelo@users.noreply.github.com>
2024-05-06 20:15:39 -0300

    agrego launch.json

--- prueba.py
+++ prueba.py
@@ -4,3 +4,10 @@ def nuevaRama():
     print("Esta es una nueva rama")
 
 nuevaRama()
+
+def factorial(n):
+    if (n==1 or n==0):
+        return 1
+    else:
+        return n * (n-1)
+factorial(5)
